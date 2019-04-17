d3f316d0de648cdb8281a567c864f0344aba1516
Jordan <jw@ylly.fr>
2019-04-17 15:47:39 +0200

    Add all return  DolistV8

--- src/DolistV8.php
+++ src/DolistV8.php
@@ -2,23 +2,26 @@
 
 namespace YllyDoListV8;
 
+use SoapClient;
+use SoapFault;
+
 class DolistV8
 {
     // Connect to DoList
-    const WSDL_CLIENT = "http://api.dolist.net/v2/AuthenticationService.svc?wsdl";
-    const SOAP_CLIENT = "http://api.dolist.net/v2/AuthenticationService.svc/soap1.1";
+    const WSDL_CLIENT = 'http://api.dolist.net/v2/AuthenticationService.svc?wsdl';
+    const SOAP_CLIENT = 'http://api.dolist.net/v2/AuthenticationService.svc/soap1.1';
 
     // Access at Segment Service
-    const WSDL_SEG = "http://api.dolist.net/v2/SegmentService.svc?wsdl";
-    const SOAP_SEG = "http://api.dolist.net/v2/SegmentService.svc/soap1.1";
+    const WSDL_SEG = 'http://api.dolist.net/v2/SegmentService.svc?wsdl';
+    const SOAP_SEG = 'http://api.dolist.net/v2/SegmentService.svc/soap1.1';
 
     // Access at Contact Service
-    const WSDL_CONTACT = "http://api.dolist.net/v2/ContactManagementService.svc?wsdl";
-    const SOAP_CONTACT = "http://api.dolist.net/v2/ContactManagementService.svc/soap1.1";
+    const WSDL_CONTACT = 'http://api.dolist.net/v2/ContactManagementService.svc?wsdl';
+    const SOAP_CONTACT = 'http://api.dolist.net/v2/ContactManagementService.svc/soap1.1';
 
     // Access at Campaign Service
-    const WSDL_CAM = "http://api.dolist.net/v2/CampaignManagementService.svc?wsdl";
-    const SOAP_CAM = "http://api.dolist.net/v2/CampaignManagementService.svc/soap1.1";
+    const WSDL_CAM = 'http://api.dolist.net/v2/CampaignManagementService.svc?wsdl';
+    const SOAP_CAM = 'http://api.dolist.net/v2/CampaignManagementService.svc/soap1.1';
 
     private $accountId;
     private $authenticationKey;
@@ -36,13 +39,14 @@ class DolistV8
 
     /**
      * @return array
+     * @throws SoapFault
      */
     public function connectDoListV8()
     {
-        $soapClient = new \SoapClient(self::WSDL_CLIENT, array("trace" => 1, "location" => self::SOAP_CLIENT));
+        $soapClient = new SoapClient(self::WSDL_CLIENT, array('trace' => 1, 'location' => self::SOAP_CLIENT));
 
-        $authenticationInfos = array("AuthenticationKey" => $this->authenticationKey, "AccountID" => $this->accountId);
-        $authenticationRequest = array("authenticationRequest" => $authenticationInfos);
+        $authenticationInfos = array('AuthenticationKey' => $this->authenticationKey, 'AccountID' => $this->accountId);
+        $authenticationRequest = array('authenticationRequest' => $authenticationInfos);
 
         $result = $soapClient->GetAuthenticationToken($authenticationRequest);
 
@@ -61,16 +65,15 @@ class DolistV8
      * @param array $options
      *
      * @return mixed
-     * @throws \SoapFault
+     * @throws SoapFault
      */
     public function send($wsdl, $soap, $function, $options)
     {
-        $soap = new \SoapClient($wsdl, array("trace" => 1, "location" => $soap));
+        $soap = new SoapClient($wsdl, array('trace' => 1, 'location' => $soap));
 
-        $result = call_user_func([$soap, $function], $options);
+        $result = $soap->$function($options);
 
         return $result;
-
     }
 
     // Part Contact //
@@ -78,7 +81,9 @@ class DolistV8
     /**
      * @param Contact $contact
      *
-     * @throws \SoapFault
+     * @return mixed
+     * 
+     * @throws SoapFault
      */
     public function saveContact(Contact $contact)
     {
@@ -87,8 +92,8 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "contact" => array(
+            'token' => $this->token,
+            'contact' => array(
                 'Email' => $contact->getEmail(),
                 'Fields' => $contact->getFields(),
                 'InterestsToAdd' => $contact->getInterestsToAdd(),
@@ -98,20 +103,18 @@ class DolistV8
             )
         );
 
-        $this->send(self::WSDL_CONTACT, self::SOAP_CONTACT, "SaveContact", $options);
-
+        return $this->send(self::WSDL_CONTACT, self::SOAP_CONTACT, 'SaveContact', $options);
     }
 
     /**
      * @param string $culture
-     * @param boolean $interest
-     * @param boolean $lastModifiedOnly
      * @param int $offset
      * @param string $email
      * @param int $memberId
      * @param array $returnFields
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function getDetailsContact($culture, $offset, $email, $memberId, $returnFields)
     {
@@ -120,22 +123,22 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "request" => array(
-                "AllFields" => true,
-                "Culture" => $culture,
-                "Interest" => true,
-                "LastModifiedOnly" => false,
-                "Offset" => $offset,
-                "RequestFilter" => array(
-                    "Email" => $email,
-                    "MemberID" => $memberId
+            'token' => $this->token,
+            'request' => array(
+                'AllFields' => true,
+                'Culture' => $culture,
+                'Interest' => true,
+                'LastModifiedOnly' => false,
+                'Offset' => $offset,
+                'RequestFilter' => array(
+                    'Email' => $email,
+                    'MemberID' => $memberId
                 ),
-                "ReturnFields" => $returnFields
+                'ReturnFields' => $returnFields
             )
         );
 
-        $this->send(self::WSDL_CONTACT, self::SOAP_CONTACT, "GetContact", $options);
+        return $this->send(self::WSDL_CONTACT, self::SOAP_CONTACT, 'GetContact', $options);
     }
 
     // Part Segment //
@@ -143,7 +146,8 @@ class DolistV8
     /**
      * @param Segment $segment
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function createSegment(Segment $segment)
     {
@@ -152,25 +156,26 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "request" => array(
-                "Culture" => $segment->getCulture(),
-                "UserEmail" => $segment->getUserEmail(),
-                "SegmentName" => $segment->getSegmentName(),
-                "RandomCount" => $segment->isRandomCount(),
-                "RandomType" => $segment->getRandomType(),
-                "RandomValue" => $segment->getRandomValue(),
-                "Groups" => $segment->getGroups()
+            'token' => $this->token,
+            'request' => array(
+                'Culture' => $segment->getCulture(),
+                'UserEmail' => $segment->getUserEmail(),
+                'SegmentName' => $segment->getSegmentName(),
+                'RandomCount' => $segment->isRandomCount(),
+                'RandomType' => $segment->getRandomType(),
+                'RandomValue' => $segment->getRandomValue(),
+                'Groups' => $segment->getGroups()
             )
         );
 
-        $this->send(self::WSDL_SEG, self::SOAP_SEG, "CreateSegment", $options);
+        return $this->send(self::WSDL_SEG, self::SOAP_SEG, 'CreateSegment', $options);
     }
 
     /**
      * @param array $emails
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function createSegmentTest($emails)
     {
@@ -179,17 +184,18 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "emails" => $emails
+            'token' => $this->token,
+            'emails' => $emails
         );
 
-        $this->send(self::WSDL_SEG, self::SOAP_SEG, "CreateSegmentTest", $options);
+        return $this->send(self::WSDL_SEG, self::SOAP_SEG, 'CreateSegmentTest', $options);
     }
 
     /**
      * @param Segment $segment
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function deleteSegment(Segment $segment)
     {
@@ -198,22 +204,23 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "request" => array(
-                "Culture" => $segment->getCulture(),
-                "UserEmail" => $segment->getUserEmail(),
-                "SegmentID" => $segment->getSegmentId(),
-                "checkIsUsedInRunningCampaigns" => true
+            'token' => $this->token,
+            'request' => array(
+                'Culture' => $segment->getCulture(),
+                'UserEmail' => $segment->getUserEmail(),
+                'SegmentID' => $segment->getSegmentId(),
+                'checkIsUsedInRunningCampaigns' => true
             )
         );
 
-        $this->send(self::WSDL_SEG, self::SOAP_SEG, "DeleteSegment", $options);
+        return $this->send(self::WSDL_SEG, self::SOAP_SEG, 'DeleteSegment', $options);
     }
 
     /**
      * @param Segment $segment
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function getSegmentById(Segment $segment)
     {
@@ -222,19 +229,19 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "request" => array(
-                "Culture" => $segment->getCulture(),
-                "UserEmail" => $segment->getUserEmail(),
-                "SegmentID" => $segment->getSegmentId(),
+            'token' => $this->token,
+            'request' => array(
+                'Culture' => $segment->getCulture(),
+                'UserEmail' => $segment->getUserEmail(),
+                'SegmentID' => $segment->getSegmentId(),
             )
         );
 
-        $this->send(self::WSDL_SEG, self::SOAP_SEG, "GetSegmentById", $options);
+        return $this->send(self::WSDL_SEG, self::SOAP_SEG, 'GetSegmentById', $options);
     }
 
     /**
-     * @throws \SoapFault
+     * @throws SoapFault
      */
     public function getAllSegments()
     {
@@ -242,7 +249,7 @@ class DolistV8
             $this->connectDoListV8();
         }
 
-        $this->send(self::WSDL_SEG, self::SOAP_SEG, "GetAllSegments", array("token" => $this->token));
+        return $this->send(self::WSDL_SEG, self::SOAP_SEG, 'GetAllSegments', array('token' => $this->token));
     }
 
     // Part Campaign //
@@ -251,7 +258,8 @@ class DolistV8
     /**
      * @param Campaign $campaign
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function createCampaign(Campaign $campaign)
     {
@@ -260,46 +268,47 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "campaignEmail" => array(
-                "Culture" => $campaign->getCulture(),
-                "FormatLinkTechnical" => $campaign->getFormatLinkTechnical(),
-                "FromAddressPrefix" => $campaign->getFromAddressPrefix(),
-                "FromName" => $campaign->getFromName(),
-                "GoogleAnalyticsTracking" => array(
-                    "GoogleProfileID" => $campaign->getGoogleProfileId(),
-                    "GoogleVariableList" => array(
-                        "ID" => $campaign->getGoogleVariableId(),
-                        "Value" => $campaign->getGoogleVariableValue()
+            'token' => $this->token,
+            'campaignEmail' => array(
+                'Culture' => $campaign->getCulture(),
+                'FormatLinkTechnical' => $campaign->getFormatLinkTechnical(),
+                'FromAddressPrefix' => $campaign->getFromAddressPrefix(),
+                'FromName' => $campaign->getFromName(),
+                'GoogleAnalyticsTracking' => array(
+                    'GoogleProfileID' => $campaign->getGoogleProfileId(),
+                    'GoogleVariableList' => array(
+                        'ID' => $campaign->getGoogleVariableId(),
+                        'Value' => $campaign->getGoogleVariableValue()
                     )
                 ),
-                "ID" => $campaign->getCampaignId(),
-                "Message" => array(
-                    "ContentHtml" => $campaign->getContentHtml(),
-                    "ContentText" => $campaign->getContentText(),
-                    "Encoding" => $campaign->getEncoding(),
-                    "Id" => $campaign->getMessageEmailId(),
-                    "MessageType" => $campaign->getMessageType(),
-                    "Name" => $campaign->getNameMessageEmail()
+                'ID' => $campaign->getCampaignId(),
+                'Message' => array(
+                    'ContentHtml' => $campaign->getContentHtml(),
+                    'ContentText' => $campaign->getContentText(),
+                    'Encoding' => $campaign->getEncoding(),
+                    'Id' => $campaign->getMessageEmailId(),
+                    'MessageType' => $campaign->getMessageType(),
+                    'Name' => $campaign->getNameMessageEmail()
                 ),
-                "PrepaidSendingMode" => $campaign->getPrepaidSendingMode(),
-                "ReplyAddress" => $campaign->getReplyAddress(),
-                "ReplyName" => $campaign->getReplyName(),
-                "Subject" => $campaign->getSubject(),
-                "TrackingDomain" => $campaign->getTrackingDomain(),
-                "UnsubscribeFormId" => $campaign->getUnsubscribeFormId(),
-                "VersionOnline" => $campaign->isVersionOnline()
+                'PrepaidSendingMode' => $campaign->getPrepaidSendingMode(),
+                'ReplyAddress' => $campaign->getReplyAddress(),
+                'ReplyName' => $campaign->getReplyName(),
+                'Subject' => $campaign->getSubject(),
+                'TrackingDomain' => $campaign->getTrackingDomain(),
+                'UnsubscribeFormId' => $campaign->getUnsubscribeFormId(),
+                'VersionOnline' => $campaign->isVersionOnline()
             )
         );
 
-        $this->send(self::WSDL_CAM, self::SOAP_CAM, "CreateCampaign", $options);
+        return $this->send(self::WSDL_CAM, self::SOAP_CAM, 'CreateCampaign', $options);
     }
 
     /**
      * @param Campaign $campaign
      * @param Segment $segment
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function sendCampaignTest(Campaign $campaign, Segment $segment)
     {
@@ -308,12 +317,12 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "campaignId" => $campaign->getCampaignId(),
-            "segmentId" => $segment->getSegmentId()
+            'token' => $this->token,
+            'campaignId' => $campaign->getCampaignId(),
+            'segmentId' => $segment->getSegmentId()
         );
 
-        $this->send(self::WSDL_CAM, self::SOAP_CAM, "SendCampaignTest", $options);
+        return $this->send(self::WSDL_CAM, self::SOAP_CAM, 'SendCampaignTest', $options);
     }
 
     /**
@@ -323,7 +332,8 @@ class DolistV8
      * @param $period
      * @param $volume
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function sendCampaign(Campaign $campaign, Segment $segment, $planning, $period, $volume)
     {
@@ -332,25 +342,26 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "campaignId" => $campaign->getCampaignId(),
-            "segmentId" => $segment->getSegmentId(),
-            "planning" => array(
-                "SendDate" => $planning
+            'token' => $this->token,
+            'campaignId' => $campaign->getCampaignId(),
+            'segmentId' => $segment->getSegmentId(),
+            'planning' => array(
+                'SendDate' => $planning
             ),
-            "frequency" => array(
-                "Period" => $period,
-                "Volume" =>$volume
+            'frequency' => array(
+                'Period' => $period,
+                'Volume' =>$volume
             )
         );
 
-        $this->send(self::WSDL_CAM, self::SOAP_CAM, "SendCampaign", $options);
+        return $this->send(self::WSDL_CAM, self::SOAP_CAM, 'SendCampaign', $options);
     }
 
     /**
      * @param Campaign $campaign
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function cancelCampaign(Campaign $campaign)
     {
@@ -359,11 +370,11 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "campaignId" => $campaign->getCampaignId()
+            'token' => $this->token,
+            'campaignId' => $campaign->getCampaignId()
         );
 
-        $this->send(self::WSDL_CAM, self::SOAP_CAM, "CancelCampaign", $options);
+        return $this->send(self::WSDL_CAM, self::SOAP_CAM, 'CancelCampaign', $options);
     }
 
     /**
@@ -371,7 +382,8 @@ class DolistV8
      * @param $last
      * @param $offset
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function getCampaignsById($date, $last, $offset)
     {
@@ -380,18 +392,18 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "request" => array(
-                "filter" => array(
-                    "AllCampaigns" => false,
-                    "Date" => $date,
-                    "LastCampaigns" => $last,
-                    "Offset" => $offset
+            'token' => $this->token,
+            'request' => array(
+                'filter' => array(
+                    'AllCampaigns' => false,
+                    'Date' => $date,
+                    'LastCampaigns' => $last,
+                    'Offset' => $offset
                 )
             )
         );
 
-        $this->send(self::WSDL_CAM, self::SOAP_CAM, "GetCampaignsID", $options);
+        return $this->send(self::WSDL_CAM, self::SOAP_CAM, 'GetCampaignsID', $options);
     }
 
     /**
@@ -399,7 +411,8 @@ class DolistV8
      * @param $last
      * @param $offset
      *
-     * @throws \SoapFault
+     * @return mixed
+     * @throws SoapFault
      */
     public function getAllCampaigns($date, $last, $offset)
     {
@@ -408,17 +421,17 @@ class DolistV8
         }
 
         $options = array(
-            "token" => $this->token,
-            "request" => array(
-                "filter" => array(
-                    "AllCampaigns" => 1,
-                    "Date" => $date,
-                    "LastCampaigns" => $last,
-                    "Offset" => $offset
+            'token' => $this->token,
+            'request' => array(
+                'filter' => array(
+                    'AllCampaigns' => 1,
+                    'Date' => $date,
+                    'LastCampaigns' => $last,
+                    'Offset' => $offset
                 )
             )
         );
 
-        $this->send(self::WSDL_CAM, self::SOAP_CAM, "GetCampaigns", $options);
+        return $this->send(self::WSDL_CAM, self::SOAP_CAM, 'GetCampaigns', $options);
     }
 }
